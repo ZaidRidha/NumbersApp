1203ddec3eb09c4139bd73e0b572d61bd9f2823e
ZaidRidha <zaidridha@yahoo.com>
2024-02-16 17:59:40 +0000

    Image corrections and adjustments

--- components/CustomHeader.js
+++ components/CustomHeader.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { View, Image, StyleSheet,TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-// Make sure to pass the navigation prop to CustomHeader
+// Assuming isTablet function is similar to the previous example
+const isTablet = () => {
+  const { width, height } = Dimensions.get("window");
+  return Math.min(width, height) >= 600;
+};
+
 const CustomHeader = ({ navigation }) => {
+  const logoStyle = isTablet() ? styles.logoTablet : styles.logo;
+
   return (
     <SafeAreaView style={styles.headerContainer}>
-      <TouchableOpacity onPress={() => navigation.navigate('LandingPage')}>
+      <TouchableOpacity onPress={() => navigation.navigate("LandingPage")}>
         <Image
-          source={require('../assets/Images/headerlogo.png')} // Replace with your logo image path
-          style={styles.logo}
+          source={require("../assets/Images/headerlogo.png")} // Replace with your logo image path
+          style={logoStyle}
           resizeMode="contain"
         />
       </TouchableOpacity>
@@ -20,18 +33,23 @@ const CustomHeader = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   headerContainer: {
-    marginTop:10,
+    marginTop: 10,
     height: 70, // Adjust the height as needed
     paddingHorizontal: 10, // Add some padding if needed
-    flexDirection: 'row', // Align children in a row
-    alignItems: 'center', // Center items vertically
-    width: '100%', // Header should span the full width
-    backgroundColor: '#fff', // Adjust the background color as needed
+    flexDirection: "row", // Align children in a row
+    alignItems: "center", // Center items vertically
+    width: "100%", // Header should span the full width
+    backgroundColor: "#fff", // Adjust the background color as needed
   },
   logo: {
     height: 100,
     width: 100,
     marginLeft: 20, // This line pushes the logo to the right
+  },
+  logoTablet: {
+    height: 150, // Larger size for tablet
+    width: 150, // Larger size for tablet
+    marginLeft: 20,
   },
 });
 
