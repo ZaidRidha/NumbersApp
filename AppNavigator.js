0257abbdd0dcbdb6e930f3113d9db8ead1d77e04
ZaidRidha <zaidridha@yahoo.com>
2024-02-26 03:14:42 +0000

    Numbers Homepage Added
    With Image carousel,

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 // Import your screen components and any additional components for the header
-import LandingPage from './screens/LandingPage.js';
-import FormulaHome from './screens/FormulaHome.js';
+import LandingPage from "./screens/LandingPage.js";
+import FormulaHome from "./screens/FormulaHome.js";
 // Import a custom header component, or define it here
-import CustomHeader from './components/CustomHeader.js';
+import CustomHeader from "./components/CustomHeader.js";
+import NumbersHome from "./screens/NumbersHome.js";
 
 const Stack = createStackNavigator();
 
@@ -13,23 +14,33 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LandingPage">
-        <Stack.Screen 
-          name="LandingPage" 
+        <Stack.Screen
+          name="LandingPage"
           component={LandingPage}
-          options={({ navigation }) => ({ // Use a function to get access to navigation
+          options={({ navigation }) => ({
+            // Use a function to get access to navigation
             header: () => <CustomHeader navigation={navigation} />, // Pass navigation prop to CustomHeader
-            cardStyle: { backgroundColor: 'white' },
+            cardStyle: { backgroundColor: "white" },
           })}
         />
-        <Stack.Screen 
-          name="FormulaHome" 
+        <Stack.Screen
+          name="FormulaHome"
           component={FormulaHome}
-          options={({ navigation }) => ({ // Use a function to get access to navigation
+          options={({ navigation }) => ({
+            // Use a function to get access to navigation
             header: () => <CustomHeader navigation={navigation} />, // Pass navigation prop to CustomHeader
-            cardStyle: { backgroundColor: 'white' },
+            cardStyle: { backgroundColor: "white" },
           })}
         />
 
+        <Stack.Screen
+          name="NumbersHome"
+          component={NumbersHome}
+          options={({ navigation }) => ({
+            header: () => <CustomHeader navigation={navigation} />,
+            cardStyle: { backgroundColor: "white" },
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
