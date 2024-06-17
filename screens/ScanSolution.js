266e9123f6d0a73a5dab1cedbb034c8f104299b6
ZaidRidha <zaidridha@yahoo.com>
2024-06-17 12:15:34 +0100

    Add "Next Question" button to ScanSolution with screen replacement navigation
    - Added a "Next Question" button to the ScanSolution component.
    - Implemented navigation.replace to navigate to ScanHome, replacing the current screen.
    - Ensured the button is styled consistently with other buttons in the app.
    - Utilized ScrollView to allow the user to scroll through the solution without showing the scroll indicator.
    
    These changes improve the user experience by allowing seamless navigation to the next question without retaining the previous screen in the navigation stack.

--- screens/ScanSolution.js
+++ screens/ScanSolution.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
-const ScanSolution = ({ route }) => {
+const ScanSolution = ({ route, navigation }) => {
   const { solution } = route.params; // Get the solution from route parameters
 
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Solution</Text>
-      <Text style={styles.solutionText}>{solution}</Text>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        showsVerticalScrollIndicator={false}
+      >
+        <Text style={styles.solutionText}>{solution}</Text>
+        <TouchableOpacity
+          style={styles.nextButton}
+          onPress={() => navigation.replace("ScanHome")}
+        >
+          <Text style={styles.actionButtonText}>Solution</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
@@ -26,13 +43,31 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     fontSize: 20,
     color: "white",
-    alignSelf: 'center',
-    textAlign: 'center',
+    alignSelf: "center",
+    textAlign: "center",
+  },
+  scrollViewContent: {
+    paddingBottom: 20,
+  },
+
+  nextButton: {
+    backgroundColor: "#538A0E",
+    padding: 10,
+    width: 200,  // Reducing the width to make the button less wide
+    alignSelf: 'center',  // This will center the button in the view
+    marginTop: 50,  // Reducing the margin to make it a bit higher
+    alignItems: "center",
+    borderRadius: 10,
   },
   solutionText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    textAlign: 'left',
+    textAlign: "left",
     marginTop: 20,
+  },
+
+  actionButtonText: {
+    color: 'white',  // Setting the text color to white
+    fontSize: 20,
   },
 });
